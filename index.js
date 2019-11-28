ebcb408403f2317841979e29711920e09580eee3
Mahuha80 <dracula01112000@gmail.com>
2019-11-28 21:51:21 +0700

    babel template

--- index.js
+++
@@ -1,8 +0,0 @@
-//how to create new user using mongodb
-// db.createUser(
-//     {
-//         user:"vinh",
-//         pwd:"vinh",
-//         roles:["readWrite","dbAdmin","dbOwner"]
-//     }
-// )
