55668850cbffdc6169509e0f2a6950b10caef15e
Mahuha80 <dracula01112000@gmail.com>
2019-11-28 21:30:59 +0700

    ok

---
+++ index.js
@@ -0,0 +1,8 @@
+//how to create new user using mongodb
+// db.createUser(
+//     {
+//         user:"vinh",
+//         pwd:"vinh",
+//         roles:["readWrite","dbAdmin","dbOwner"]
+//     }
+// )
